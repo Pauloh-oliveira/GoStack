0d9e782e966b8feaa545b7ee2c9e3a3af296bde1
Paulo Henrique <paulohenrique_789@hotmail.com>
2021-01-17 21:56:47 -0300

    Entendendo propriedades do React

--- conceitos-dev/frontend/src/App.js
+++ conceitos-dev/frontend/src/App.js
@@ -5,8 +5,18 @@ import Header from './components/Header';
 function App() {
     return (
         <>
-        <Header />
-        <Header />
+        <Header title="Homepage"> 
+            <ul>
+                <li>Homepage</li>
+                <li>Projects</li>
+            </ul>
+        </Header>
+        <Header title="Projects"> 
+            <ul>
+                <li>pagina inicial</li>
+                <li>Projetos</li>
+            </ul>
+        </Header>
         </>
     );
 }
