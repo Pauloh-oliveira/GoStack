ff7d3fdbd538593eb31eb2e570fa4333af8100a5
Paulo Henrique <paulohenrique_789@hotmail.com>
2021-01-23 21:29:29 -0300

    conectando o mobile com api

--- conceitos-dev/mobile/src/index.js
+++ conceitos-dev/mobile/src/index.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View,FlatList, Text, StyleSheet, StatusBar, SafeAreaView } from 'react-native';
+
+import api from './services/api'
 
 export default function App() {
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        });
+    }, [])
+
     return (
         <>
             <StatusBar barStyle="dark-content" backgroundColor="#7159c1"  />
-            <View style={styles.container}>
-                <Text style={styles.title}>Hello GoStack</Text>
-            </View>    
+            <SafeAreaView style={styles.container}>
+                <FlatList 
+                    
+                    data={projects}    
+                    keyExtractor={project => project.id}
+                    renderItem={({ item: project}) => (
+                        <Text style={styles.project}>{project.title}</Text>
+                    )}
+                />
+            </SafeAreaView>
+            {/* <View style={styles.container}>
+                { projects.map(project => 
+                (<Text style={styles.project} key={project.id}>{project.title}</Text>))}
+            </View>     */}
         </>    
     );
 }
@@ -16,12 +38,9 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#7159c1',
-        justifyContent: 'center',
-        alignItems: 'center'
     },
-    title: {
+    project: {
         color: '#fff',
-        fontSize: 32,
-        fontWeight: 'bold'
+        fontSize: 20,
     },
 })
